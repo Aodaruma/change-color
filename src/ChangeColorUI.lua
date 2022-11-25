a52c8b8f6577427893c674a45cbaa571d7870cbe
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2022-11-25 14:00:35 +0900

    Create ChangeColorUI.lua

---
+++ src/ChangeColorUI.lua
@@ -0,0 +1,12 @@
+--[[
+
+WARNING: place this file into the same folder as the exedit.auf (mainly plugins folder or scripts folder)
+
+
+this file is part of ChangeColor.anm, which is developed by Aodaruma.
+mainly used for color space interpolation.
+if you want to use this file, please keep the header and follow the license of ChangeColor.anm script.
+
+by Aodaruma (twitter: @Aodaruma_)
+]]
+
